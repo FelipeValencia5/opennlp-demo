46e0d073b179ec33f2b395a99039dfb913a95f09
Felipe Alejandro Valencia Bacca <fvalenci@ull.edu.es>
2019-05-10 13:02:16 +0100

    Añadido: - Funcionalidad para leer archivos de texto - Estructura de carpetas de Test - Dependencias de JUnit en archivo POM

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -1,8 +1,10 @@
-
 package org.fogbeam.example.opennlp;
 
-
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,53 +13,74 @@ import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 
 
-public class TokenizerMain
-{
-	public static void main( String[] args ) throws Exception
-	{
-		
-		// the provided model
+public class TokenizerMain{
+
+	String texto="";
+
+	public void leerArchivo() {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;		
+		try {
+			archivo = new File ("src/main/resources/archivo.txt");
+			fr = new FileReader (archivo);
+			br = new BufferedReader(fr);
+			String linea;
+			while((linea=br.readLine())!=null) {
+				texto=texto+linea;
+			}
+		}
+		catch(Exception e){
+			e.getMessage();
+		}finally{
+			try{
+				if( null != fr ){
+					fr.close();
+				}
+			}catch (Exception e){
+				e.getMessage();
+			}
+		}		
+	}
+
+	public void Tokenizer() throws Exception{
+		// The Provided Model
 		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
 
-		
-		// the model we trained
+		// The model we trained
 		InputStream modelIn = new FileInputStream( "models/en-token.model" );
-		
-		try
-		{
+
+		try{
 			TokenizerModel model = new TokenizerModel( modelIn );
-		
 			Tokenizer tokenizer = new TokenizerME(model);
-			
-				/* note what happens with the "three depending on which model you use */
+			/* note what happens with the "three depending on which model you use */
 			String[] tokens = tokenizer.tokenize
-					(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, " 
-							+ " mentions \"three Mounts raised by the Indians over three of their Great Kings" 
-							+ " who were killed in the Wars.\"" );
-			
-			for( String token : tokens )
-			{
+					(texto);
+
+			for( String token : tokens ){
 				System.out.println( token );
 			}
-			
 		}
-		catch( IOException e )
-		{
+		catch( IOException e ){
 			e.printStackTrace();
 		}
-		finally
-		{
-			if( modelIn != null )
-			{
-				try
-				{
+		finally{
+			if( modelIn != null ){
+				try{
 					modelIn.close();
 				}
-				catch( IOException e )
-				{
+				catch( IOException e ){
+					e.getMessage();
 				}
 			}
 		}
+		
 		System.out.println( "\n-----\ndone" );
+	}
+
+	public static void main( String[] args ) throws Exception{
+		TokenizerMain tokenizerMain = new TokenizerMain();
+		tokenizerMain.leerArchivo();
+		tokenizerMain.Tokenizer();
 	}
 }
---
+++ src/test/java/org/fogbeam/example/opennlp/test/TestTokenizerMain.java
@@ -0,0 +1,17 @@
+package org.fogbeam.example.opennlp.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.fogbeam.example.opennlp.TokenizerMain;
+import org.junit.Test;
+
+public class TestTokenizerMain {
+	
+	private TokenizerMain tokenizerMain= new TokenizerMain();
+	
+	@Test
+	public void leerFichero() {
+		
+	}
+
+}
